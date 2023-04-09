56dc329e6c6d213b98d112a40b4c1b04ff2ef7f1
Kalyan Mudumby <71078441+theinhumaneme@users.noreply.github.com>
2023-04-10 03:14:37 +0530

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,2 +1,2 @@
-sudo apt update &&  sudo apt install docker.io docker-compose git
+sudo apt update &&  sudo apt install docker.io docker-compose git -y
 git clone https://github.com/theinhumaneme/major && cd major && docker-compose up -d
